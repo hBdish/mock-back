f55085e98ec23fd12aabbfd2e0e536a6e8482822
Maksim Doronkin <maksim3602@gmail.com>
2023-10-01 16:10:39 +0300

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const jsonServer = require('json-server');
+const path = require('path');
+
+const server = jsonServer.create();
+
+const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
+
+server.use(jsonServer.defaults({}));
+server.use(jsonServer.bodyParser);
+
+
+server.use(router);
+
+// запуск сервера
+server.listen(8000, () => {
+  console.log('server is running on 8000 port');
+});
